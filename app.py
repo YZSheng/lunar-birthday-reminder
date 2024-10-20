aed4815392b04a6424dbbfbdc2b96ec814c8e8c3
Sheng Yunzhou <shengyunzhou@gmail.com>
2024-10-20 14:45:23 +0800

    Add basic date calculation

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-import datetime
-from lunarcalendar import Converter, Solar, Lunar, DateNotExist
+from datetime import datetime
+import yaml
+from lunarcalendar import Converter, Lunar
 
-solar = Solar(2018, 1, 1)
-print(solar)
-lunar = Converter.Solar2Lunar(solar)
-print(lunar)
-solar = Converter.Lunar2Solar(lunar)
-print(solar)
-print(solar.to_date(), type(solar.to_date()))
+
+def calculate_solar_dates():
+    with open("config/dates.yaml", "r") as f:
+        file = yaml.safe_load(f)
+        birthdays = file.get("birthdays")
+        for birthday in birthdays:
+            lunar = Lunar(datetime.now().year, birthday.get('lunar_month'), birthday.get('lunar_date'))
+            solar = Converter.Lunar2Solar(lunar)
+            print(f"Birthday: {birthday['name']}, Lunar: {lunar}, Solar: {solar}")
+
+
+if __name__ == "__main__":
+    calculate_solar_dates()
