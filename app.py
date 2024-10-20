3b43998da44b52b7bfaa806a9b4cd9935011cd23
Sheng Yunzhou <shengyunzhou@gmail.com>
2024-10-20 15:31:24 +0800

    Derive the next 10 years

--- app.py
+++ app.py
@@ -2,16 +2,25 @@ from datetime import datetime
 import yaml
 from lunarcalendar import Converter, Lunar
 
+this_year = datetime.now().year
 
-def calculate_solar_dates():
+
+def add_calendar_event(name, lunar_birthday):
+    for i in range(10):
+        lunar = Lunar(
+            this_year + i, lunar_birthday["lunar_month"], lunar_birthday["lunar_date"]
+        )
+        solar = Converter.Lunar2Solar(lunar)
+        print(f"{name}: {solar}")
+
+
+def mark_future_birthdays():
     with open("config/dates.yaml", "r") as f:
         file = yaml.safe_load(f)
         birthdays = file.get("birthdays")
         for birthday in birthdays:
-            lunar = Lunar(datetime.now().year, birthday.get('lunar_month'), birthday.get('lunar_date'))
-            solar = Converter.Lunar2Solar(lunar)
-            print(f"Birthday: {birthday['name']}, Lunar: {lunar}, Solar: {solar}")
+            add_calendar_event(birthday["name"], birthday)
 
 
 if __name__ == "__main__":
-    calculate_solar_dates()
+    mark_future_birthdays()
