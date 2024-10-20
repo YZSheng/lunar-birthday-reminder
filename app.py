993b8407b33a55112b7e1b81ee5ccb409a4457bc
Sheng Yunzhou <shengyunzhou@gmail.com>
2024-10-20 15:59:39 +0800

    Dump output to a file

--- app.py
+++ app.py
@@ -6,12 +6,17 @@ this_year = datetime.now().year
 
 
 def add_calendar_event(name, lunar_birthday):
+    solar_birthdays = []
     for i in range(10):
         lunar = Lunar(
             this_year + i, lunar_birthday["lunar_month"], lunar_birthday["lunar_date"]
         )
         solar = Converter.Lunar2Solar(lunar)
-        print(f"{name}: {solar}")
+        solar_birthdays.append(solar.to_date())
+    with open("config/solar_birthday.yaml", "w") as file:
+        yaml.dump(
+            solar_birthdays, file, default_flow_style=False
+        )  # default_flow_style=False makes the YAML more readable
 
 
 def mark_future_birthdays():
